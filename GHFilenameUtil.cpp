c1ab26af4f78a1d811daf6d48ac8331d7fd554b5
david <djw@goldenhammersoftware.com>
2019-01-26 13:31:36 -0500

    replace extension version that doesn't make new memory
    Signed-off-by: david <djw@goldenhammersoftware.com>

--- GHFilenameUtil.cpp
+++ GHFilenameUtil.cpp
@@ -2,11 +2,27 @@
 #include <assert.h>
 #include <string.h> // strchr
 #include <stdio.h> // sprintf
+#include <algorithm> // min
 
 char* GHFilenameUtil::replaceExtension(const char* input, const char* ext)
 {
 	assert(input != 0);
 	assert(ext != 0);
+
+	// Slightly sloppy to avoid code duplication.
+	// Create a new buffer of size input + ext + 1(\0) to avoid duplication.
+	// Code that is worried about efficiency should be calling the other version of this function.
+	size_t bufSize = strlen(input) + strlen(ext) + 1;
+	char* ret = new char[bufSize];
+	replaceExtension(input, ext, ret, bufSize);
+	return ret;
+}
+
+void GHFilenameUtil::replaceExtension(const char* input, const char* ext, char* out, size_t outLen)
+{
+	assert(input != 0);
+	assert(ext != 0);
+	assert(out != 0);
 
 	const char* existingExt = strchr(input, '.');
 	size_t preExtLen = 0;
@@ -16,15 +32,16 @@ char* GHFilenameUtil::replaceExtension(const char* input, const char* ext)
 	}
 	else
 	{
+		// if we didn't find a . then append our new extension to the end of the string.
 		preExtLen = strlen(input);
 	}
 	size_t newExtLen = strlen(ext);
 
-    // +1 for '.'
+	// +1 for '.'
 	size_t newStrLen = preExtLen + 1 + newExtLen;
-	char* ret = new char[newStrLen+1]; // +1 for \0
-	snprintf(ret, preExtLen+1, "%s", input);
-	sprintf(ret, "%s.%s", ret, ext);
-
-	return ret;
+	assert(newStrLen < outLen);
+	snprintf(out, std::min(preExtLen + 1, outLen), "%s", input);
+	snprintf(out, outLen, "%s%s", out, ext);
+	// paranoia
+	out[outLen - 1] = '\0';
 }
--- GHFilenameUtil.h
+++ GHFilenameUtil.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <stddef.h> // size_t
+
 // A place for common filename manipulation functions.
 namespace GHFilenameUtil
 {
 	// Replace any existing extension or add a new one if not found.
 	// Return new memory.
 	char* replaceExtension(const char* input, const char* ext);
+	// Replace any existing extension or add a new one if not found.
+	// Copy result into out.
+	void replaceExtension(const char* input, const char* ext, char* out, size_t outLen);
 }
